88e9b41f98c4ba4ce3569df9d5426579a0d19106
Cody1001 <codybrant@me.com>
2017-07-06 11:32:10 -0400

    Commented out most stuff.

--- game/urls.py
+++ game/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'game'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^play/$', views.play, name='play'),
-    url(r'^results/$', views.results, name='results'),
+#    url(r'^play/$', views.play, name='play'),
+#    url(r'^results/$', views.results, name='results'),
 ]
